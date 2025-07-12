d4bd50af3a410e3f56a29a1430e0b3873b1c3359
Uwemski <iconuwemfrank@gmail.com>
2025-07-12 12:05:34 +0100

    added dockerfile for testing

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM php:8.2-cli
+
+RUN apt-get update && apt-get install -y \
+    git unzip zip curl libzip-dev libpng-dev libonig-dev libxml2-dev \
+    && docker-php-ext-install pdo pdo_mysql zip
+
+COPY --from=composer:2 /usr/bin/composer /usr/bin/composer
+
+WORKDIR /var/www
+
+COPY . .
+
+RUN composer install --no-dev --optimize-autoloader
+
+RUN chmod -R 755 storage bootstrap/cache
+
+ENV PORT=8080
+EXPOSE 8080
+
+CMD php -S 0.0.0.0:${PORT} -t public
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
   <title>Parish Locator | Home Page</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
-  <link rel="stylesheet" href="{{asste('style.css')}}" />
+  <link rel="stylesheet" href="{{asset('style.css')}}" />
   <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}" type="image/x-icon"/>
 </head>
 <body>
