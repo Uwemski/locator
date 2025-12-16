8d199d20a67584fe47ca055cc7bf9bedbbb9213c
Uwemski <iconuwemfrank@gmail.com>
2025-12-16 12:57:18 +0100

    As usual, added component driven programming style to admin pages and inputed the links for pagination to all paginated pages for now

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -1,141 +1,65 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="dash.css">
-    <title>Document</title>
-    <style>
-        .info {
-            font-size: 1.5rem;
-            text-align: left;
-        }
+
+<x-admin-layout>
     
-    </style>
-</head>
-<body>
-    <div class="container-fluid">
 
-   
-    <!-- <div class="container"> -->
-        <!-- <div class="row">
-            <div class="col-md-3 mt-4 mb-3"><a href="products.php" class=>View products</a></div>
-        </div>
-        <div class="row">
-            <div class="col-md-3 mb-3"><a href="customers.php">View Customers</a></div>
-        </div>
-        <div class="row">
-            <div class="col-md-3 mb-3"><a href="waste.php">View Wastes</a></div>
-        </div> -->
-        
-        <div class="row">
-            <div class="col-md-3">
-                <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px;height: 100vh;">
-                    <ul class="nav nav-pills flex-column mb-auto">
-                        <li class="nav-item">
-                            <a href="{{route('admin.all_users')}}" class="nav-link active" aria-current="page">
-                            <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#users"></use></svg>
-                            View Users
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{route('admin.all_parish')}}" class="nav-link text-white">
-                            <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#parishes"></use></svg>
-                            View Parish
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{route('admin.active_parish')}}" class="nav-link text-white">
-                            <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
-                            Verifed Parishes
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{route('admin.unverified')}}" class="nav-link text-white">
-                            <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
-                            Un-verifed Parishes
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{route('admin.suspended')}}" class="nav-link text-white">
-                            <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
-                            Suspended Parishes
-                            </a>
-                        </li>
-                        <li>
-                            <form action="/adminLogout" method="post">
-                                <!--I keep forgetting to put csrf, nna ehn!-->
-                                @csrf
-                                <button class="btn btn-danger ml-3">Log Out</button>
-                            </form>
-                        </li>
-                        
+    <div class="col-md-9">
+        <h2 class="mt-4">Welcome to your dashboard <?php //it will be sexy if you can echo the customer's fullname on his dahsboard ?></h2>
+            {{-- @foreach ($parishes as $parish)
                     
-                    </ul>
+            @endforeach --}}
+
+        <div class="d-flex d-flex-row justify-content-center">
+            <div class="card mt-3 bg-success mx-1" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Number Of Registerd Parishes</h5>
+                    <p class="card-text text-center" style="font-size: 3rem; font-weight: 700">{{$parishes}}</p>
+                    {{-- <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a> --}}
                 </div>
             </div>
-            <div class="col-md-9">
-                <h2 class="mt-4">Welcome to your dashboard <?php //it will be sexy if you can echo the customer's fullname on his dahsboard ?></h2>
-                {{-- @foreach ($parishes as $parish)
-                    
-                @endforeach --}}
 
-                <div class="d-flex d-flex-row justify-content-center">
-                    <div class="card mt-3 bg-success mx-1" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">Number Of Registerd Parishes</h5>
-                            <p class="card-text text-center" style="font-size: 3rem; font-weight: 700">{{$parishes}}</p>
+            <div class="card mt-4 bg-primary mx-1" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Number Of Registerd Users</h5>
+                    <p class="card-text text-center" style="font-size: 3rem; font-weight: 700">{{$users}}</p>
                             {{-- <a href="#" class="card-link">Card link</a>
                             <a href="#" class="card-link">Another link</a> --}}
-                        </div>
-                    </div>
-
-                    <div class="card mt-4 bg-primary mx-1" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">Number Of Registerd Users</h5>
-                            <p class="card-text text-center" style="font-size: 3rem; font-weight: 700">{{$users}}</p>
-                            {{-- <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a> --}}
-                        </div>
-                    </div>
-
-                    <div class="card mt-5 bg-secondary mx-1" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">Number Of Verified Parishes</h5>
-                            <p class="card-text text-center" style="font-size: 3rem;  font-weight: 700">{{$verifiedParishes}}</p>
-                            {{-- <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a> --}}
-                        </div>
-                    </div>
                 </div>
+            </div>
+
+            <div class="card mt-5 bg-secondary mx-1" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Number Of Verified Parishes</h5>
+                    <p class="card-text text-center" style="font-size: 3rem;  font-weight: 700">{{$verifiedParishes}}</p>
+                            {{-- <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a> --}}
+                </div>
+            </div>
+        </div>
                 
 
 
-                @if(session('error'))
-                    <div class="alert alert-danger">{{session('error')}}</div>
-                @endif
+        @if(session('error'))
+            <div class="alert alert-danger">{{session('error')}}</div>
+        @endif
                 
-                @if ($errors->any())
-                    @foreach ($errors->all() as $err )
-                        <div class="alert-warning mt-3">{{$err}}</div>
-                    @endforeach
-                @endif
-                <div class="col-md-5 info pt-3">
-                    <form action="/admin/search" method="GET">
-                        @csrf
-                        <label>Enter name of parish</label>
-                        <input type='text' name='name' id='name' required placeholder='Enter name of parish here' class='form-control mt-2' value={{old('name')}}>
-                        @error('name')
-                            <small class="alert alert-warning">{{$message}}</small>
-                        @enderror
+        @if ($errors->any())
+            @foreach ($errors->all() as $err )
+                <div class="alert-warning mt-3">{{$err}}</div>
+            @endforeach
+        @endif
+        <div class="col-md-5 info pt-3">
+            <form action="/admin/search" method="GET">
+                @csrf
+                <label>Enter name of parish</label>
+                <input type='text' name='search' id='name' required placeholder='Enter name of parish here' class='form-control mt-2' value={{old('name')}}>
+                @error('name')
+                    <small class="alert alert-warning">{{$message}}</small>
+                @enderror
 
-                        <button class='btn btn-primary mt-2'>Search</button>
-                    </form>
-                </div>
-            </div>
+                <button class='btn btn-primary mt-2'>Search</button>
+            </form>
         </div>
     </div>
-</body>
-</html>
+</x-admin-layout>
+
--- resources/views/admin/all_parish.blade.php
+++ resources/views/admin/all_parish.blade.php
@@ -1,74 +1,68 @@
-<?php
-    //use App\Models\Parish;
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <title>All Parish</title>
-</head>
-<body>
-    <!--Upon parish registration, admin should be able to view it from here-->
+<x-admin-layout>
 
-    <div class="container">
-        <div class="row mt-4">
-            <table border=1 class="table table-hover"> 
-                <thead>
-                    <tr>
-                        <th>S/N</th>
-                        <th>Parish Name</th>
-                        <th>Parish Location</th>
-                        <th>Parish Email</th>
-                        <th>Date Registered</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php $serialNo = 1 ?>
-                    @foreach ($parishes  as $p)
-                        
-                        <tr>
-                            <td>{{ $serialNo }}</td>
-                            <td>{{$p->name}}</td>
-                            <td>{{$p->address}}</td>
-                            <td>{{$p->email}}</td>
-                            <td>{{$p->created_at->format('Y-m-d')}}</td>
-                            <td>{{$p->status}}</td>
-                            <td>
-                                <form action="{{route('parish.update', $p->id)}}" method="POST">
-                                    @csrf
-                                    @method('PUT')
-                                    <select name="status" class="form-select">
-                                        <option value="pending" {{$p->status == 'pending'? 'selected': ''}}>Pending</option>
-                                        <option value="verified" {{$p->status == 'verified'? 'selected': ''}}>Verify</option>
-                                        <option value="suspended" {{$p->status == 'suspended'? 'selected': ''}}>Suspended</option>
-                                    </select>
-                                    <button type="submit" class="btn btn-success mt-1">update</button>
-                                </form>
-                            </td>
-                            <td>
-                                <form action="{{route('parish.destroy', $p->id)}}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="btn btn-danger">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
 
-                        
-                        <?php $serialNo++ ?>
-                    @endforeach
-                </tbody>
-            </table>
+    <div class="row mt-4">
+        <div class="">
+            <form action="{{route('admin.search')}} " method="GET">
+                @csrf
+                <div class="mb-3">
+                    <input type="hidden" name="status" value="verified" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <input type="text" name="search" class="form-control">
+                </div>
+                <button class="btn btn-primary">Search</button>
+            </form>
         </div>
-        {{ $parishes->links()}}
+
+        <table border=1 class="table table-hover"> 
+            <thead>
+                <tr>
+                    <th>S/N</th>
+                    <th>Parish Name</th>
+                    <th>Parish Location</th>
+                    <th>Parish Email</th>
+                    <th>Date Registered</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $serialNo = 1 ?>
+                @foreach ($parishes  as $p)            
+                <tr>
+                    <td>{{ $serialNo }}</td>
+                    <td>{{$p->name}}</td>
+                    <td>{{$p->address}}</td>
+                    <td>{{$p->email}}</td>
+                    <td>{{$p->created_at->format('Y-m-d')}}</td>
+                    <td>{{$p->status}}</td>
+                    <td>
+                        <form action="{{route('parish.update', $p->id)}}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <select name="status" class="form-select">
+                                <option value="pending" {{$p->status == 'pending'? 'selected': ''}}>Pending</option>
+                                <option value="verified" {{$p->status == 'verified'? 'selected': ''}}>Verify</option>
+                                <option value="suspended" {{$p->status == 'suspended'? 'selected': ''}}>Suspended</option>
+                            </select>
+                            <button type="submit" class="btn btn-success mt-1">update</button>
+                        </form>
+                        </td>
+                        <td>
+                            <form action="{{route('parish.destroy', $p->id)}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger">Delete</button>
+                            </form>
+                        </td>
+                </tr>
+                <?php $serialNo++ ?>
+                @endforeach
+            </tbody>
+        </table>
+            {{$parishes->links()}}
     </div>
-</body>
-</html>
+</x-admin-layout>
