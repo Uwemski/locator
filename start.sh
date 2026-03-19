06697746fbb0d3b00a24f05463652e4c10e04c35
Uwemski <iconuwemfrank@gmail.com>
2026-03-19 14:47:05 +0100

    commit

--- start.sh
+++ start.sh
@@ -1,16 +1,14 @@
-#!/usr/bin/env bash
+#!/usr//bin/env bash
+
 echo "Running composer"
-composer global require hirak/prestissimo
+
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
-
 echo "Caching config..."
-php artisan config:cache
+php artisan config:clear
 
-echo "Caching routes..."
-php artisan route:cache
+echo "Caching route"
+php artisan route:clear
 
 echo "Publishing cloudinary provider..."
 php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider" --tag="cloudinary-laravel-config"
